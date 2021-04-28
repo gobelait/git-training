bd5d9b5124b2aaf83dbad06d0f7ecca42161445a
Guillaume <guillaume@sogilis.com>
2021-04-28 16:19:43 +0200

    Add samurai sauce aliment

--- sandwich_maker.py
+++ sandwich_maker.py
@@ -25,6 +25,11 @@ class Ketchup(Aliment):
         super().__init__(Nature.SAUCE, "Ketchup")
 
 
+class Samurai(Aliment):
+    def __init__(self):
+        super().__init__(Nature.SAUCE, "Samurai")
+
+
 class Butter(Aliment):
     def __init__(self):
         super().__init__(Nature.DAIRY, 'Butter')
